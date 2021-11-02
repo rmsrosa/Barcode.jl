f0d365c9c9a94b7eb6141a12902bac20b97fdbbc
Ricardo Rosa <rmsrosa@gmail.com>
2021-11-02 09:19:11 -0300

    pattern to Gray image

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,10 @@
+"""
+    pattern_img(binary_pattern, height = 20)
+
+Return an Gray Image BitArray with the number of rows specified by `height` and
+the `binary_pattern` filling the rows with 0's and 1's. Displaying the image reveals
+the barcode.
+"""
 function pattern_img(binary_pattern, height = 20)
     bp = prod(binary_pattern)
     img = Gray.(
