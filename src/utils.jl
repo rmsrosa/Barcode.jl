9b57a8967c67fea08984e8d5e72d3e5ce993fbc3
Ricardo Rosa <rmsrosa@gmail.com>
2021-11-02 18:17:06 -0300

    incread img_height default and add paddings

--- src/utils.jl
+++ src/utils.jl
@@ -1,25 +1,31 @@
 """
-    pattern_img(binary_pattern; img_height = 20)
+    pattern_img(binary_pattern; img_height = 50)
 
 Return an image of type `Matrix{Gray{Bool}}` where the number of rows is specified
 by `img_height` and where the rows are filled with the `binary_pattern`. Displaying
 the image reveals the barcode.
 """
-function pattern_img(binary_pattern; img_height = 20)
-    bp = prod(binary_pattern)
+function pattern_img(binary_pattern; img_height = 50)
+    cbp = collect(prod(binary_pattern))
     # with a little help from @ScottPJones and @borodi
-    img = Gray.(repeat(reshape(map(==('0'), collect(bp)), 1,:), img_height, 1))
+    img = Gray.(
+        [
+            fill(true, 1,  length(cbp));
+            repeat(reshape(map(==('0'), cbp), 1,:), img_height, 1);
+            fill(true, 1, length(cbp))
+        ]
+    )
     return img
 end
 
 """
-    pattern_img(filename, binary_pattern; img_height = 20)
+    pattern_img(filename, binary_pattern; img_height = 50)
 
 Generate an image of type `Matrix{Gray{Bool}}` where the number of rows is specified
 by `img_height` and where the rows are filled with the `binary_pattern` and save it
 to `filename` using any format accepted by `FileIO`.
 """
-function pattern_img(filename, binary_pattern; img_height = 20)
+function pattern_img(filename, binary_pattern; img_height = 50)
     img = pattern_img(binary_pattern; img_height)
     FileIO.save(filename, img)
 end
